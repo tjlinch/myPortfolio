98be990ed59a36647ebc522065c9209126473544
tjlinch <tjlinch@gmail.com>
2022-11-07 10:48:10 -0700

    format changes

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import NavBar from './NavBar';
+import './Header.css';
 
 function Header() {
   return (
-    <div>
+    <div className='header'>
         <h1>Tyler Linch</h1>
         <div>
             <NavBar />
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,28 +1,45 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import icons8Burger from '../assets/icons8-beef-burger-60.png';
+import './NavBar.css';
 
 function NavBar() {
   return (
-    <div>
-        <button className="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">
-            <img src='https://img.icons8.com/ios-glyphs/344/beefburger.png' alt='Menu'></img>
+    <div className='navBar'>
+       {/* fullscreen nav list */}
+        <div>
+            <ul>
+                <Link to='/'>
+                    <li className='list-group-item'>Home</li>
+                </Link>
+                <Link to='/portfolio'>
+                    <li className='list-group-item'>Projects</li>
+                </Link>
+            </ul>
+        </div>
+        
+        {/* mobile nav list in an offcanvas menu */}
+        <button className="btn btn-light" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">
+            {/* TODO: link to burger icon on references page */}
+            {/* <a target="_blank" href="https://icons8.com/icon/95244/beef-burger">Beef Burger</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a> */}
+            <img src={icons8Burger} alt='Nav menu'></img>
         </button>
 
         <div className="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
-        <div className="offcanvas-header">
-            <h5 className="offcanvas-title" id="offcanvasRightLabel">Offcanvas right</h5>
-            <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-        </div>
-        <div className="offcanvas-body">
-           <ul className='list-group'>
-            <Link to='/'>
-                <li className='list-group-item'>Home</li>
-            </Link>
-            <Link to='/portfolio'>
-                <li className='list-group-item'>Projects</li>
-            </Link>
-           </ul>
-        </div>
+            <div className="offcanvas-header">
+                <h5 className="offcanvas-title" id="offcanvasRightLabel">Offcanvas right</h5>
+                <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div className="offcanvas-body">
+            <ul className='list-group'>
+                <Link to='/'>
+                    <li className='list-group-item'>Home</li>
+                </Link>
+                <Link to='/portfolio'>
+                    <li className='list-group-item'>Projects</li>
+                </Link>
+            </ul>
+            </div>
         </div>
     </div>
   );
