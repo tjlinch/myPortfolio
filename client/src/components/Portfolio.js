819e3eeeb018cc890dd302d3503411998868f321
tjlinch <tjlinch@gmail.com>
2022-11-16 11:08:19 -0700

    projects page

--- client/src/components/Portfolio.js
+++ client/src/components/Portfolio.js
@@ -1,8 +1,26 @@
-import React from 'react'
+import React from 'react';
+import './Portfolio.css';
+import projects from '../assets/json_files/projects.json';
 
 function Portfolio() {
   return (
-    <div>Portfolio</div>
+    <div>
+      <h3>Portfolio</h3>
+      <div className='container'>
+        {projects.map((project) => (
+          <div className='card'>
+            <h4 className='cardHeader'>{project.name}</h4>
+            <img className='image' src={project.image} alt='application screenshot'></img>
+            <a href={project.deployed} target='_blank' rel='noreferrer'>
+              <button type='button' className='btn btn-primary'>Deployed App</button>
+            </a>
+            <a href={project.github} target='_blank' rel='noreferrer'>
+              <button type='button' className='btn btn-secondary'>GitHub Repository</button>
+            </a>
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
 
