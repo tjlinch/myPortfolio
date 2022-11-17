6c53842fb2aa4bae4d3793d854e5a02d0ce8d581
tjlinch <tjlinch@gmail.com>
2022-11-17 13:53:23 -0700

    contact page w/ emailjs

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -1,10 +1,67 @@
 import React from 'react';
+import { useState } from 'react';
+import { send } from 'emailjs-com';
 import './Contact.css';
 
 const Contact = () => {
+  
+  const [toSend, setToSend] = useState({
+    from_name: '',
+    message: '',
+    reply_to: '',
+  });
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    send(
+      'service_zma9qzm',
+      'template_b7bi61y',
+      toSend,
+      'andvVFNKwMuDuNLGU'
+    )
+      .then((response) => {
+        console.log('email submitted', response.status, response.text);
+      })
+      .catch((err) => {
+        console.log('email failed', err);
+      });
+  };
+
+  const handleChange = (event) => {
+    setToSend({ ...toSend, [event.target.name]: event.target.value });
+  };
+  
   return (
     <div className='contact'>
       <h3>Contact Information</h3>
+      <div className='contactInstructions'>
+        <p>Please fill out the form below to contact me via email. I can also be reached by phone at 720-202-0206.</p>
+      </div>
+    <form className='emailForm' onSubmit={onSubmit}>
+      <input
+        type='text'
+        name='from_name'
+        placeholder='Your Name'
+        value={toSend.from_name}
+        onChange={handleChange}
+      />
+      <input
+        type='text'
+        name='reply_to'
+        placeholder='Your Email'
+        value={toSend.reply_to}
+        onChange={handleChange}
+      />
+      <input
+        type='text'
+        name='message'
+        placeholder='Your Message'
+        value={toSend.message}
+        onChange={handleChange}
+        className='messageInput'
+      />
+      <button type='submit' className='btn btn-primary'>submit</button>
+    </form>
     </div>
   )
 }
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -10,8 +10,10 @@ function Home() {
       <div className='photoContainer'>
         <img className='homePhoto' src='https://media-exp1.licdn.com/dms/image/C5603AQG8BqXAiDV_OQ/profile-displayphoto-shrink_800_800/0/1517229610292?e=1671062400&v=beta&t=XBeXkSlJJikmLAmAMYbFf3mpAXnZtKqaitoJ4Wub_sE' alt='LinkedIn profile'></img>
       </div>
-      <p className='homeParagraph'>Thanks for visiting my portfolio site. This application will serve as a place to present my current and future projects in software development.</p>
-      <p className='homeParagraph'>You can also find links to my other online profiles in the footer, as well as contact me directly through the Contact page.</p>
+      <div className='paragraphContainer'>
+        <p className='homeParagraph'>Thanks for visiting my portfolio site. This application will serve as a place to present my current and future projects in software development.</p>
+        <p className='homeParagraph'>You can also find links to my other online profiles in the footer, as well as contact me directly through the Contact page.</p>
+      </div>
       <div className='technologiesContainer'>
         {technologies.map((technology) => (
           <div className='techCard'>
