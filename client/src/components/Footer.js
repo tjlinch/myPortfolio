defa46d1d37a543f5a59df7be1c854bc919a64a0
tjlinch <tjlinch@gmail.com>
2022-11-17 11:28:20 -0700

    icon disclaimer in footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -12,6 +12,7 @@ function Footer() {
       <a href='https://www.linkedin.com/in/tjlinch47/' target='_blank' rel='noreferrer'>
         <img id='linkedin' src={icons8linkedin} alt='linkedin logo'></img>
       </a>
+      <p className='iconDisclaimer'>*Icons by<a target="_blank" rel="noreferrer" href="https://icons8.com">Icons8</a></p>
     </div>
   )
 }
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -20,7 +20,6 @@ function Home() {
           </div>
         ))}
       </div>
-
     </div>
   )
 }
