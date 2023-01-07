6420fb5ac16ed3d13c6aae244c121e12f200dcfc
tjlinch <tjlinch@gmail.com>
2023-01-06 18:25:17 -0700

    homepage photo fix

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Home.css';
 import technologies from '../assets/json_files/technologies.json';
+import homePhoto from '../assets/images/IMG_2335.jpg';
 
 
 function Home() {
@@ -8,7 +9,7 @@ function Home() {
     <div className='home'>
       <h3>Welcome to my Portfolio!</h3>
       <div className='photoContainer'>
-        <img className='homePhoto' src='https://media-exp1.licdn.com/dms/image/C5603AQG8BqXAiDV_OQ/profile-displayphoto-shrink_800_800/0/1517229610292?e=1671062400&v=beta&t=XBeXkSlJJikmLAmAMYbFf3mpAXnZtKqaitoJ4Wub_sE' alt='LinkedIn profile'></img>
+        <img className='homePhoto' src={homePhoto} alt='Tyler Linch'></img>
       </div>
       <div className='paragraphContainer'>
         <p className='homeParagraph'>Thanks for visiting my portfolio site. This application will serve as a place to present my current and future projects in software development.</p>
